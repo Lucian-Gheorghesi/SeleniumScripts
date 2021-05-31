49143bf0eac65bcbebe22c8531abdac6b7e800cc
Lucian-Gheorghesi <85110656+Lucian-Gheorghesi@users.noreply.github.com>
2021-05-31 22:21:40 +0300

    Add files via upload

---
+++ CONDITION2.py
@@ -0,0 +1,11 @@
+name = "John"
+age = 27
+if name == "John" and age == 27:
+   # print("Your name is John, and you are also 23 years old.")
+   pass
+
+if name == "John" or name == "Rick":
+    print("Your name is either John or Rick.")
+
+if name in ["John", "Rick"]:
+    print("Your name is either John or Rick.")
